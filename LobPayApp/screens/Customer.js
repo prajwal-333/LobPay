65c7d119d8a8f43377c1a2d60a16b81ee8be583c
prajwal-333 <prajwalhg333@gmail.com>
2020-06-27 12:22:58 +0530

    nothing

---
+++ LobPayApp/screens/Customer.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,TextInput,Button ,TouchableOpacity} from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import Signup from './Signup';
+
+
+export class Customer extends Component{
+    render(){
+        return(
+            <View>
+                
+            </View>
+        );
+    }
+}
