6379a61d7085d306eee73de2e70c93716d63890a
SrikarMannepalli <37926341+SrikarMannepalli@users.noreply.github.com>
2020-07-01 10:43:32 +0530

    Update urls.py

--- backend/Invoices/urls.py
+++ backend/Invoices/urls.py
@@ -4,8 +4,8 @@ from .views import fillMerchant
 from .views import createBills
 
 urlpatterns = [
-    path("fillinv/", fillInventory.as_view(), name="Fill the inventory table"),
-    path("fillMerchantInv/<mid>", fillMerchant.as_view(), name="Fill the merchant's inventory status"),
-    path("createBill/<cid>/<mid>",createBills.as_view(), name="Customer's bills")
+    path("inventory/", fillInventory.as_view(), name="Fill the inventory table"),
+    path("merchantInventory/<mid>", fillMerchant.as_view(), name="Fill the merchant's inventory status"),
+    path("bills/<cid>/<mid>",createBills.as_view(), name="Customer's bills")
 ]
 
